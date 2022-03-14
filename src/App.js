7d9b35cf303a2756a687f302bb359430d5cfbb84
Robert Turkot <thiswarjustgottenfeethigh@gmail.com>
2022-03-14 16:19:24 +0000

    added sky

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
 class App extends Component{
 	componentDidMount() {
 		let scene = new THREE.Scene();
+		altidudecol =  new THREE.Color( 0, 0, 0 );
+		scene.background = altidudecol
 		let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+		let curraltitude = 0
 		let renderer = renderer = new THREE.WebGLRenderer( { antialias: true } );
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		this.mount.appendChild( renderer.domElement );
 		let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-
+		var clock = new THREE.Clock();
 		camera.position.set(0,5,15);
 
 		let loader = new STLLoader();
@@ -45,7 +47,7 @@ class App extends Component{
 					change = 0 
 					return 0
 				}
-				return change
+				return diff-change
 			}
 			else {
 				return 0 
@@ -56,10 +58,17 @@ class App extends Component{
 
 		function animate() {
 				requestAnimationFrame( animate );
+				delta = clock.getDelta();
 				if(mesh) {
-
+					mesh.rotation.x = linearise(delta, curpos, 0, rotation)
+					mesh.rotation.y = linearise(delta, curpos, 1, rotation)
+					mesh.rotation.z = linearise(delta, curpos, 2, rotation)
 				}
-				
+				if(curraltitude != altidude) {
+					altidudecol = (0, 0, 500/altidude)
+					scene.background = altidudecol
+					curraltitude = altidude  
+				}
 
 				renderer.render( scene, camera );
 		};
